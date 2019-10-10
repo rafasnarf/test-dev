099c935abf7f15a3aa16bccd315561203d526dd5
rafasnarf <snarfelcv@gmail.com>
2019-10-09 23:12:34 -0300

    Backend: Inclusão e Pesquisa de dados

--- destino.php
+++ destino.php
@@ -2,35 +2,78 @@
 <body>
 <?php
 
-if(isset($_POST['marca']) && isset($_POST['modelo']) && isset($_POST['ano']))
-{
-  cadastro();
-} elseif (isset($_GET)) {
-  listar();
-};
+    require __DIR__ . '/vendor/autoload.php';
 
+    use JmesPath\search;
 
- function cadastro(){
-    $json = file_get_contents('carros.json');
-    $data = json_decode($json, true);
+    if(isset($_POST['marca']) && isset($_POST['modelo']) && isset($_POST['ano']))
+    {
+      cadastro();
+    } elseif (isset($_GET) && $_GET['idProcura'] == '') 
+    {
+      listar();
+    } elseif (isset($_GET) && $_GET['idProcura'] != '')
+    {
+      procura();
+    };
 
-    array_push($data, array (
-      'id'=>'10'.count($data)+1, 
-      'marca' => $_POST['marca'], 
-      'modelo' => $_POST['modelo'],  
-      'ano' => $_POST['ano'], 
-      'created' => date("Y-m-d H:i:s"), 
-      'modified' => date("Y-m-d H:i:s")
-    ));
-    $data = json_encode($data);
-    $json = file_put_contents('carros.json', $data);
-  }
+  function cadastro(){
+      $json = file_get_contents('carros.json');
+      $data = json_decode($json, true);
+
+     array_push($data, array (
+       'id'=> count($data)+1, 
+       'marca' => $_POST['marca'], 
+       'modelo' => $_POST['modelo'],  
+       'ano' => $_POST['ano']
+      ));
+      $data = json_encode($data);
+      $json = file_put_contents('carros.json', $data);
+    }
 
   function listar(){
     $json = file_get_contents('carros.json');
     $data = json_decode($json , true);
 
+    foreach ($data as $row) {
+        foreach($row as $campo => $valor){
+          echo '<div>' . $campo . " " . $valor . '</div>';
+        }
+    }
+  }
 
+  function procura(){
+  
+    $json = file_get_contents('carros.json');
+    $data = json_decode($json , true);
+  
+    $procura = $_GET['idProcura'];
+    
+    $expression = "[? id == `$procura`]";
+  
+    $result = JmesPath\search($expression, $data);
+
+    if($result == null )
+    {
+        echo "Não há registro com este número de ID";
+    } else 
+    {
+        foreach ($result as $row) {
+          foreach($row as $campo => $valor){
+             echo '<div>' . $campo . " " . $valor . '</div>';
+          }
+        }
+        echo '<div id="alterar">
+                <form action="destino.php" method="PUT">
+                  <input type="submit" value="alterar">
+                </form>
+              </div>';
+        echo '<div id="excluir">
+                <form action="destino.php" method="DELETE">  
+                  <input type="submit" value="excluir"> 
+                </form>
+              </div>';
+    } 
   }
 ?>  
 </body>
---
+++ teste2.php
@@ -0,0 +1,22 @@
+<?php
+  
+  require __DIR__ . '/vendor/autoload.php';
+
+  use JmesPath\search;
+
+  $json = file_get_contents('carros_2.json');
+  $data = json_decode($json , true);
+
+  $procura = $_GET['idProcura'];
+  
+  $expression = "[? id == `$procura`]";
+
+  $result = JmesPath\search($expression, $data);
+
+   foreach ($result as $row) {
+      foreach($row as $campo => $valor){
+          echo '<div>' . $campo . " " . $valor . '</div>';
+    }
+  } 
+echo '<div id="alterar"><input type="submit" value="alterar"></div>';
+echo '<div id="excluir"><input type="submit" value="excluir"></div>';
